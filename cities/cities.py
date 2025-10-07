ef790c2ec13ccc518d92882d577ccf2dba34edb5
ChrisCortesSanchez <79801197+ChrisCortesSanchez@users.noreply.github.com>
2025-10-07 18:49:20 -0400

    Added num_cities and create functions

--- cities/cities.py
+++ cities/cities.py
@@ -1,5 +1,7 @@
 # File for city level data
 
+from random import randint
+
 MIN_ID_LEN = 1
 
 ID = "id"
@@ -17,3 +19,16 @@ def is_valid_id(id:str) -> bool:
         return False
     return True
 
+def num_cities()->int:
+    return len(city_cache)
+
+
+def create(fields:dict)->str:
+    if not(isinstance(fields, dict)):
+        raise ValueError(f"Invalid type for {type(fields)=}")
+    if not fields.get(NAME):
+        raise ValueError(f"Invalid value for {fields.get(NAME)=}")
+    
+    new_id = str(len(city_cache) + 1)
+    city_cache[new_id] = fields
+    return new_id

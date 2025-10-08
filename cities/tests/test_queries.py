2081ad702a45d46e86103bea90be6b2a0d7f2f3b
Angel Mejia <am12850@nyu.edu>
2025-10-07 22:25:26 -0400

    Completed basic tests for test_queries.py

---
+++ cities/tests/test_queries.py
@@ -0,0 +1,33 @@
+import pytest
+
+import cities.queries as qry
+
+
+@pytest.mark.skip('This is an example of a bad test!')
+def test_bad_test_for_num_cities():
+    assert qry.num_cities() == len(qry.city_cache)
+
+
+def test_num_cities():
+    # get the count
+    old_count = qry.num_cities()
+    # add a record
+    qry.create(qry.SAMPLE_CITY)
+    assert qry.num_cities() == old_count + 1
+
+
+def test_good_create():
+    old_count = qry.num_cities()
+    new_rec_id = qry.create(qry.SAMPLE_CITY)
+    assert qry.is_valid_id(new_rec_id)
+    assert qry.num_cities() == old_count + 1
+
+
+def test_create_bad_name():
+    with pytest.raises(ValueError):
+        qry.create({})
+
+
+def test_create_bad_param_type():
+    with pytest.raises(ValueError):
+      qry.create(17)

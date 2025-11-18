564c1929a34f15e4c3a95d4b109344d4cc4e2900
ChrisCortesSanchez <cc7825@nyu.edu>
2025-11-18 18:50:44 -0500

    Fixed some tests in test_queries to allow for compatability with the database

--- cities/tests/test_queries.py
+++ cities/tests/test_queries.py
@@ -18,6 +18,16 @@ def temp_city_no_del():
 def test_bad_test_for_num_cities():
     assert qry.num_cities() == len(qry.city_cache)
 
+@pytest.fixture(scope='function')
+def temp_city():
+    temp_rec = get_temp_rec()
+    new_rec_id = qry.create(get_temp_rec())
+    yield new_rec_id
+    try:
+        qry.delete(temp_rec[qry.NAME], temp_rec[qry.STATE_CODE])
+    except ValueError:
+        print('The record was already deleted.')
+
 
 def test_num_cities():
     # get the count
@@ -29,8 +39,7 @@ def test_num_cities():
 
 def test_good_create():
     old_count = qry.num_cities()
-    new_rec_id = qry.create(qry.SAMPLE_CITY)
-    assert qry.is_valid_id(new_rec_id)
+    qry.create(get_temp_rec())
     assert qry.num_cities() == old_count + 1
 
 
@@ -43,11 +52,11 @@ def test_create_bad_param_type():
     with pytest.raises(ValueError):
       qry.create(17)
 
-@patch('cities.queries.db_connect', return_value=True, autospec=True)
-def test_read(mock_db_connect):
+def test_read(temp_city):
     cities = qry.read()
-    assert isinstance(cities, dict)
-    assert temp_city in cities
+    assert isinstance(cities, list)
+    assert get_temp_rec() in cities
+
 
 @patch('cities.queries.db_connect', return_value=False, autospec=True)
 def test_read_cant_connect(mock_db_connect):
@@ -69,7 +78,7 @@ def test_delete(temp_city_no_del):
 @patch('cities.queries.db_connect', return_value=True, autospec=True)
 def test_delete_not_there(mock_db_connect):
     with pytest.raises(ValueError):
-        qry.delete('some value that is not there')
+        qry.delete('some city name that is not there', 'not a state')
 
 
 # Additional fixture for creating multiple cities

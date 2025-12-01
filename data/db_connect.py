3d2b294ac0e088657d662ed0e00370460192a00c
ChrisCortesSanchez <cc7825@nyu.edu>
2025-11-30 21:10:43 -0500

    Connected MongoDB to the cloud

--- data/db_connect.py
+++ data/db_connect.py
@@ -53,9 +53,7 @@ def connect_db():
                 raise ValueError('You must set your password '
                                  + 'to use Mongo in the cloud.')
             print('Connecting to Mongo in the cloud.')
-            client = pm.MongoClient(f'mongodb+srv://gcallah:{password}'
-                                    + '@koukoumongo1.yud9b.mongodb.net/'
-                                    + '?retryWrites=true&w=majority')
+            client = pm.MongoClient(f'mongodb+srv://cc7825_db_user:QO20FAO2dORMT3Yp@cluster0.8paq7vn.mongodb.net/?appName=Cluster0')
         else:
             print("Connecting to Mongo locally.")
             client = pm.MongoClient()

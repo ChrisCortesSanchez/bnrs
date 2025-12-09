3be7ad5af0cfcd095dd36395202e5a1b95be7b80
ChrisCortesSanchez <cc7825@nyu.edu>
2025-12-09 13:00:09 -0500

    Update deploy.sh to ssh into server

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # This shell script deploys a new version to a server.
 
-PROJ_DIR=demo-repo4
-VENV=Fall2023
-PA_DOMAIN="Fall2023.pythonanywhere.com"
-PA_USER='Fall2023'
+PROJ_DIR=bnrs
+VENV=venv
+PA_DOMAIN="bnrs.pythonanywhere.com"
+PA_USER='bnrs'
+PA_API_TOKEN = 19275349cbb7f23da8004df8c56ebc61306bd889
 echo "Project dir = $PROJ_DIR"
 echo "PA domain = $PA_DOMAIN"
 echo "Virtual env = $VENV"
 
-if [ -z "$DEMO_PA_PWD" ]
+if [ -z "$BNRS_PA_PWD" ]
 then
-    echo "The PythonAnywhere password var (DEMO_PA_PWD) must be set in the env."
+    echo "The PythonAnywhere password var (BNRS_PA_PWD) must be set in the env."
     exit 1
 fi
 
 echo "PA user = $PA_USER"
-echo "PA password = $DEMO_PA_PWD"
+echo "PA password = $BNRS_PA_PWD"
 
 echo "SSHing to PythonAnywhere."
-sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
+sshpass -p $BNRS_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
     cd ~/$PROJ_DIR; PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN ./rebuild.sh
 EOF
